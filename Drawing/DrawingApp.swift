c1aa4e77c0a04e64e72f40ec42e49fbf9a77e926
Weerawut <weerawut@live.com>
2024-11-29 08:35:56 +0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Weerawut Chaiyasomboon on 29/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
